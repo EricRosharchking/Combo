3bdbbf39880f5241417c26b75ee4c7028d99c918
soonwan.mui.2013 <soonwan.mui.2013@sis.smu.edu.sg>
2015-12-13 23:29:47 +0800

    EditLyrics Java Class
    Added codes for sharing music scores to edit lyrics page. will continue
    after my androidstudio finishes installing sdk updates.

--- app/src/main/java/com/example/liyuan/projectcombo/EditLyrics.java
+++ app/src/main/java/com/example/liyuan/projectcombo/EditLyrics.java
@@ -1,7 +1,78 @@
 package com.example.liyuan.projectcombo;
 
-/**
- * Created by Teresa on 13/12/2015.
- */
-public class EditLyrics {
+import java.util.List;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class EditLyrics extends Activity {
+    private final static String ARG_INT = "ARG_INT";
+
+    private String stringField;
+    private int intField;
+    private List<Object> arrayField;
+
+    private enum DataHolder {
+        INSTANCE;
+
+        private List<Object> mObjectList;
+
+        public static boolean hasData() {
+            return INSTANCE.mObjectList != null;
+        }
+
+        public static void setData(final List<Object> objectList) {
+            INSTANCE.mObjectList = objectList;
+        }
+
+        public static List<Object> getData() {
+            final List<Object> retList = INSTANCE.mObjectList;
+            INSTANCE.mObjectList = null;
+            return retList;
+        }
+    }
+
+    @Override
+    protected void onCreate(final Bundle savedState) {
+        super.onCreate(savedState);
+
+        // Get the activity intent if there is a one
+        final Intent intent = getIntent();
+
+        // And retrieve arguments if there are any
+        if (intent.hasExtra(ARG_STRING)) {
+            stringField = intent.getExtras().getString(ARG_STRING);
+        }
+        if (intent.hasExtra(ARG_INT)) {
+            intField = intent.getExtras().getInt(ARG_INT);
+        }
+        // And we retrieve large data from enum
+        if (DataHolder.hasData()) {
+            arrayField = DataHolder.getData();
+        }
+
+        // Now stringField, intField fields are available
+        // within the class and can be accessed directly
+    }
+
+
+    public static void startActivity(final Context context, final String stringArg,
+                                     final int intArg, final List<Object> objectList) {
+
+        // Initialize a new intent
+        final Intent intent = new Intent(context, SomeActivity.class);
+
+        // To speed things up :)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+
+        // And add arguments to the Intent
+        intent.putExtra(ARG_STRING, stringArg);
+        intent.putExtra(ARG_INT, intArg);
+
+        // Now we put the large data into our enum instead of using Intent extras
+        DataHolder.setData(objectList);
+
+        context.startActivity(intent);
+    }
 }
