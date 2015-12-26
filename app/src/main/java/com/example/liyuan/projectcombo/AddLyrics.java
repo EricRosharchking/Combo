8b325f04a48decbb7f89c16c70cf60874bd53998
soonwan.mui.2013 <soonwan.mui.2013@sis.smu.edu.sg>
2015-12-26 20:55:50 +0800

    Add lyrics
    added some codes

--- app/src/main/java/com/example/liyuan/projectcombo/AddLyrics.java
+++ app/src/main/java/com/example/liyuan/projectcombo/AddLyrics.java
@@ -25,17 +25,36 @@ import java.util.List;
 public class AddLyrics extends ActionBarActivity {
 
     TableLayout tbLayout;
+    TextView tvScores1;
+    EditText etLyrics1;
+    TextView tvScores2;
+    EditText etLyrics2;
+    TextView tvScores3;
+    EditText etLyrics3;
+    TextView tvScores4;
+    EditText etLyrics4;
+    Button btnSaveLyrics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_lyrics);
 
+        tvScores1 = (TextView) findViewById(R.id.textViewScores1);
+        etLyrics1 = (EditText) findViewById(R.id.editTextLyrics1);
+        tvScores2 = (TextView) findViewById(R.id.textViewScores2);
+        etLyrics2 = (EditText) findViewById(R.id.editTextLyrics2);
+        tvScores3 = (TextView) findViewById(R.id.textViewScores3);
+        etLyrics3 = (EditText) findViewById(R.id.editTextLyrics3);
+        tvScores4 = (TextView) findViewById(R.id.textViewScores4);
+        etLyrics4 = (EditText) findViewById(R.id.editTextLyrics4);
+        btnSaveLyrics = (Button) findViewById(R.id.buttonSaveLyrics);
 
         //retrieve scores (in String) from MainActivity
         Intent intent = getIntent();
         String rawScores = intent.getStringExtra("scores");
 
+		
         //String scores = rawScores.replace("_", " ");
 
         //tvScores1.setText(scores);
@@ -121,6 +140,83 @@ public class AddLyrics extends ActionBarActivity {
                 }
             }
         });*/
+		
+        String scores = rawScores.replace("_", " ");
+
+        String[] toDisplay = split(scores);
+        tvScores1.setText(scores);
+
+        //save to pdf
+        btnSaveLyrics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FileOutputStream out = null;
+
+                try{
+                    String data = tvScores1.getText().toString().trim();
+
+                    String filename = "Song";
+
+                    out = getApplicationContext().openFileOutput(filename, Context.MODE_PRIVATE);
+
+                    //data is your string or byte block or whatever
+                    out.write(data.getBytes(Charset.forName("UTF-8")));
+
+                    out.close();
+                } catch(FileNotFoundException e){
+                    e.printStackTrace();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    //split the String to String[] for use to split into multiple arrays
+    //split the String to String[] to ensure alignment with the lyrics text box
+    //(i.e. one lyrics tb under one score)
+    private static String[] split(String string)
+    {
+        char[] chars = string.toCharArray();
+        String[] strings = new String[chars.length];
+        for (int i = 0; i < chars.length; i++)
+        {
+            strings[i] = String.valueOf(chars[i]);
+        }
+        return strings;
+    }
+
+    //split the String[] into multiple arrays to ensure that one array fits into one score text view
+    //(i.e. one score line of size X is in textviewscores1)
+    public static <T extends Object> List<T[]> splitArray(T[] array, int max){
+
+        int x = array.length / max;
+        int r = (array.length % max); // remainder
+
+        int lower = 0;
+        int upper = 0;
+
+        List<T[]> list = new ArrayList<T[]>();
+
+        int i=0;
+
+        for(i=0; i<x; i++){
+
+            upper += max;
+
+            list.add(Arrays.copyOfRange(array, lower, upper));
+
+            lower = upper;
+        }
+
+        if(r > 0){
+
+            list.add(Arrays.copyOfRange(array, lower, (lower + r)));
+
+        }
+
+        return list;
     }
 
     //split the String to String[] for use to split into multiple arrays
